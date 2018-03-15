913a782aa2648dc60ef3b608c341964f9312b54a
Jordan Watson <jordan.watson@verint.com>
2018-03-15 11:57:17 +0000

    Remove pointless api file

--- src/script/api.js
+++
@@ -1,9 +0,0 @@
-import openSocket from 'socket.io-client';
-// Socket
-const socket = openSocket('http://localhost:8000');
-
-function whatsTheChat(chat) {
-	socket.on('chat', chat => cb(null, chat));
-	socket.emit('chatter', 1000);
-}
-export { whatsTheChat };
